32abc22d2e9422a0d3ec2e1546dffda1e9b23cfd
Joe Flowers <jflowers45@gmail.com>
2016-01-12 18:56:17 -0500

    make some prev/next data available

--- packages/custom/rizers/server/controllers/profile.js
+++ packages/custom/rizers/server/controllers/profile.js
@@ -15,7 +15,25 @@ module.exports = function(System){
 			res.json(rizeAPI.getAllProfiles());
 		},
 		showOne:function(req,res) {
-			res.json(rizeAPI.getOneProfile(req.params.id));
+			var account= rizeAPI.getOneProfile(req.params.id);
+			var category=rizeAPI.getOneCategory(account.profile.category)
+
+			for (var i=0; i < category.accounts.length;i++) {
+				if (category.accounts[i].id==account.id) {
+					var nextAccountIndex=i+1;
+					var prevAccountIndex=i-1;
+					if (i==0) {
+						prevAccountIndex=category.accounts.length-1;
+					} else if (i==(category.accounts.length-1)) {
+						nextAccountIndex=0;
+					}
+					var prev = category.accounts[prevAccountIndex];
+					var next = category.accounts[nextAccountIndex];
+					account.prevAccount={display_name:prev.display_name,id:prev.id, profileImage:prev.profile.profileImage, image:prev.profile.image}
+					account.nextAccount={display_name:next.display_name,id:next.id, profileImage:next.profile.profileImage, image:next.profile.image}
+				}
+			}
+			res.json(account);
 		}
 	};
 };
